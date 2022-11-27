c456671dca7ed49cc12e1d49e470da12cdf6eeff
PRONOY <pronoybanik82@gmail.com>
2022-11-27 21:03:04 +0600

    all ans add

--- src/component/Pages/Home/Blogs/Blogs.js
+++ src/component/Pages/Home/Blogs/Blogs.js
@@ -18,7 +18,17 @@ const Blogs = () => {
                  <p>The Prototypal Inheritance is a feature in javascript used to add methods and properties in objects. It is a method by which an object can inherit the properties and methods of another object. Traditionally, in order to get and set the  of an object, we use Object.</p>
                 </div>
 
-                
+                <div className='border-solid border-2 border-sky-500 p-2 broder-round'>
+                    <h1 className='font-bold'>3.  What is a unit test? Why should we write unit tests?</h1>
+                    <p>The main objective of unit testing is to isolate written code to test and determine if it works as intended. Unit testing is an important step in the development process, because if done correctly, it can help detect early flaws in code which may be more difficult to find in later testing stages.</p>
+
+                </div>
+
+                <div className='border-solid border-2 border-sky-500 p-2 broder-round'>
+                    <h1 className='font-bold'>4.  React vs. Angular vs. Vue? </h1>
+                    <div>Vue provides higher customizability and hence is easier to learn than Angular or React. Further, Vue has an overlap with Angular and React with respect to their functionality like the use of components. Hence, the transition to Vue from either of the two is an easy option</div>
+
+                </div>
             </div>
         </div>
     );
