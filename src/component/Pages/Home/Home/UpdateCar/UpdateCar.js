dc3acb3e11adfd7d1141f77bde628f93a813ccf4
PRONOY <pronoybanik82@gmail.com>
2022-12-14 12:22:03 +0600

    first commit

--- src/component/Pages/Home/Home/UpdateCar/UpdateCar.js
+++ src/component/Pages/Home/Home/UpdateCar/UpdateCar.js
@@ -13,7 +13,7 @@ const UpdateCar = () => {
 
     return (
         <div >
-            <h2 className='my-20 text-center text-3xl text-gray-600 font-semibold font-serif'>Latest News & Reviews</h2>
+            <h2 className='my-20 text-center text-3xl text-gray-600 font-semibold font-serif'>Latest News & Reviews car</h2>
             <div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 ml-2'>
                 {
                     items.map(item => <Cars
